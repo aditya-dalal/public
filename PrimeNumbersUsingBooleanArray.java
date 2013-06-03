bc689ac30aec7a957afb60bda11cf65172b58c97
Aditya Dalal <aditya.dalal@flipkart.com>
2013-06-03 17:47:44 +0530

    Find prime numbers upto N where N < 10**8

---
+++ PrimeNumbersUsingBooleanArray.java
@@ -0,0 +1,53 @@
+package Test;
+
+import java.util.Scanner;
+
+public class PrimeNumbersUsingBooleanArray
+{
+    static boolean[] numberList;
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int limit = Integer.parseInt(scanner.nextLine());
+        numberList = new boolean[limit];
+
+        long startTime = System.nanoTime();
+        System.out.println(findPrimes(limit));
+        long totalTime = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Total time(ms): " + totalTime);
+    }
+
+    private static int findPrimes(int number)
+    {
+        for(int i=0; i < number; i++)
+        {
+            numberList[i] = true;
+        }
+
+        numberList[0] = false;
+        for(int value = 1; value < numberList.length; value ++)
+        {
+            if(numberList[value] == false)
+                continue;
+            else
+            {
+                for(int i = 2; i < numberList.length; i++)
+                {
+                    int index = ((value + 1) * i) - 1;
+                    if(index >= (numberList.length))
+                        break;
+                    numberList[index] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for(Boolean value : numberList)
+        {
+            if(value == true)
+                count++;
+        }
+        return count;
+    }
+}
---
+++ PrimeNumbersUsingIntegerArray.java
@@ -0,0 +1,52 @@
+package Test;
+
+import java.util.Scanner;
+
+public class PrimeNumbersUsingIntegerArray
+{
+    static int[] numberList;
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int limit = Integer.parseInt(scanner.nextLine());
+        numberList = new int[limit];
+
+        long startTime = System.nanoTime();
+        System.out.println(findPrimes(limit));
+        long totalTime = (System.nanoTime() - startTime) / 1000000;
+        System.out.println("Total time(ms): " + totalTime);
+    }
+
+    private static int findPrimes(int number)
+    {
+        for(int i=0; i < number; i++)
+        {
+            numberList[i] = i+1;
+        }
+
+        numberList[0] = 0;
+        for(Integer value : numberList)
+        {
+            if(value == 0)
+                continue;
+            else
+            {
+                for(int i = 2; i < numberList.length; i++)
+                {
+                    int index = (value * i) - 1;
+                    if(index >= (numberList.length))
+                        break;
+                    numberList[index] = 0;
+                }
+            }
+        }
+
+        int count = 0;
+        for(Integer value : numberList)
+        {
+            if(value != 0)
+                count++;
+        }
+        return count;
+    }
+}
