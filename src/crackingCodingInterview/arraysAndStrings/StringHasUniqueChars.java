547d1bf2de32b4484fd87e0ba6a512d27958cb15
Aditya Dalal <aditya.dalal@flipkart.com>
2014-05-15 16:23:34 +0530

    Solution for unique chars using sort

--- src/crackingCodingInterview/arraysAndStrings/StringHasUniqueChars.java
+++ src/crackingCodingInterview/arraysAndStrings/StringHasUniqueChars.java
@@ -1,43 +1,48 @@
 package crackingCodingInterview.arraysAndStrings;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class StringHasUniqueChars
 {
-	public static void main(String[] args)
-	{
-		String str = "abcdef";
-		System.out.println(bruteForce(str));
-		System.out.println(booleanArray(str));
+    public static void main(String[] args)
+    {
+        String str = "abcdefa";
+        System.out.println(bruteForce(str));
+        System.out.println(booleanArray(str));
         System.out.println(usingBitManipulation(str));  // Works only for alphabets
+        System.out.println(usingSort(str));
     }
 
-	public static boolean bruteForce(String str)
-	{
-		int length = str.length();
-		for(int i = 0; i < length; i++)
-		{
-			for(int j = i + 1; j < length; j++)
-			{
-				if(str.charAt(i) == str.charAt(j))
-				{
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    public static boolean bruteForce(String str)
+    {
+        int length = str.length();
+        for(int i = 0; i < length; i++)
+        {
+            for(int j = i + 1; j < length; j++)
+            {
+                if(str.charAt(i) == str.charAt(j))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-	public static boolean booleanArray(String str)
-	{
-		boolean[] boolArr = new boolean[256];
-		for(char val : str.toCharArray())
-		{
-			if(boolArr[(int) val] == true)
-				return false;
-			else
-				boolArr[(int) val] = true;
-		}
-		return true;
-	}
+    public static boolean booleanArray(String str)
+    {
+        boolean[] boolArr = new boolean[256];
+        for(char val : str.toCharArray())
+        {
+            if(boolArr[(int) val] == true)
+                return false;
+            else
+                boolArr[(int) val] = true;
+        }
+        return true;
+    }
 
     public static boolean usingBitManipulation(String str)
     {
@@ -49,6 +54,20 @@ public class StringHasUniqueChars
             if((checker & temp) > 0)
                 return false;
             checker |= temp;
+        }
+        return true;
+    }
+
+    public static boolean usingSort(String str)
+    {
+        List<Character> charArray = new ArrayList<Character>();
+        for(char val : str.toCharArray())
+            charArray.add(val);
+        Collections.sort(charArray);
+        for(int i = 0; i < str.length() - 1; i++)
+        {
+            if(charArray.get(i) == charArray.get(i+1))
+                return false;
         }
         return true;
     }
