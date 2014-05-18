2dbd1be2cef717c7772c5624f0eb3199a718db1d
Aditya Dalal <aditya.dalal@flipkart.com>
2014-05-18 16:33:14 +0530

    Solution to replace spaces in string

---
+++ src/crackingCodingInterview/arraysAndStrings/ReplaceSpacesInString.java
@@ -0,0 +1,31 @@
+package crackingCodingInterview.arraysAndStrings;
+
+public class ReplaceSpacesInString
+{
+    public static void main(String[] args)
+    {
+        String str = " ab c";
+        String replace = "%20";
+
+        System.out.println(replaceSpacesUsingInbuiltFunction(str, replace));
+        System.out.println(replaceSpacesWith(str, replace));
+    }
+
+    public static String replaceSpacesUsingInbuiltFunction(String str, String replace)
+    {
+        return str.replaceAll("\\s", replace);
+    }
+
+    public static String replaceSpacesWith(String str, String replace)
+    {
+        StringBuilder result = new StringBuilder();
+        for(char val : str.toCharArray())
+        {
+            if(val == ' ')
+                result.append(replace);
+            else
+                result.append(val);
+        }
+        return result.toString();
+    }
+}
